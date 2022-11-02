8c945ba28e19b8103f56f1959fa9f2166800315b
Victor Batista <victorbtst77@gmail.com>
2022-11-02 20:31:16 -0300

    prototipo da página inicial

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,23 +1,98 @@
-import { StatusBar} from 'react-native'
-import styled from 'styled-components/native';
-
+import { StatusBar } from "react-native";
+import styled from "styled-components/native";
 
 export const PageContainer = styled.View`
-flex: 1;
-padding: 12px;
-padding-top: ${StatusBar.currentHeight}px;
-`; 
+  flex: 1;
+  padding: 16px;
+  padding-top: ${StatusBar.currentHeight}px;
+`;
 
-export const Greetings = styled.View`
-    margin-top: 48px;
-`
+export const Greetings = styled.View``;
 
 export const Title = styled.Text`
-    font-size: 48px;
-    color: white;
-    font-weight: bold;
+  font-size: 48px;
+  color: white;
+  font-weight: bold;
+`;
+
+export const SubTitle = styled.Text`
+  font-size: 32px;
+  color: white;
+  font-weight: bold;
+  margin: 6px;
+`;
+
+
+export const GreetingsContainer = styled.View`
+  margin: 48px 0px 64px 0px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Profile = styled.View`
+  width: 70px;
+  height: 70px;
+  border-radius: 999px;
+  background-color: #f2f2f2;
+`;
+
+export const SectionTitle = styled.Text`
+  margin-bottom: 18px;
+  font-size: 24px;
+  color: #fff;
+`;
+
+export const LastMeals = styled.View`
+  margin-top: 48px;
+`;
+
+export const MealItem = styled.View`
+  width: 150px;
+  height: 200px;
+  background-color: #f2f2f2;
+  margin-right: 16px;
+  border-radius: 16px;
+`;
+
+export const FoodTracking = styled.View`
+  margin-top: 24px;
+  background-color: #313131;
+  border-radius: 16px;
+  padding: 16px;
+  min-height: 100px;
+`;
+
+export const FTHeaderContainer = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 16px;
+  justify-content: space-between;
+`;
+
+export const Info = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Content = styled.View`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
 `
 
+export const FoodTrackingIcon = styled.View`
+  width: 30px;
+  height: 30px;
+  border-radius: 999px;
+  background-color: #f2f2f2;
+`;
+
 export const HomeLogo = styled.Image`
-    width: fit-content;
-`
+  width: fit-content;
+`;
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,16 +1,74 @@
-import { View } from 'react-native'
-import { Greetings, PageContainer, Title } from '../components/Container'
-import { Text } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import { View, ScrollView } from "react-native";
+import {
+  FoodTracking,
+  FTHeaderContainer,
+  Info,
+  Content,
+  FoodTrackingIcon,
+  Greetings,
+  LastMeals,
+  SectionTitle,
+  GreetingsContainer,
+  MealItem,
+  Profile,
+  PageContainer,
+  Title,
+  SubTitle,
+} from "../components/Container";
+import { Text } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
 
-
-export default function Home({ navigation }: RootTabScreenProps<'Home'>) {
+export default function Home({ navigation }: RootTabScreenProps<"Home">) {
   return (
     <PageContainer>
+      <GreetingsContainer>
         <Greetings>
           <Text>Bem vindo de volta</Text>
-          <Title >Paçoca</Title>
+          <Title>Paçoca</Title>
         </Greetings>
+        <Profile />
+      </GreetingsContainer>
+
+
+
+
+      <Text>Última Medição em 14/05/22</Text>
+      <FoodTracking>
+        <FTHeaderContainer>
+
+          <Content>
+            <Text>Nível de Gordura</Text>
+
+            <Info>
+              <FoodTrackingIcon />
+              <SubTitle>90,1</SubTitle>
+              <Text>kg</Text>
+            </Info>
+          </Content>
+
+          <Content>
+            <Text>TMB</Text>
+
+            <Info>
+              <FoodTrackingIcon />
+              <SubTitle>1500</SubTitle>
+              <Text>kcal</Text>
+            </Info>
+          </Content>
+
+        </FTHeaderContainer>
+      </FoodTracking>
+
+
+      <LastMeals>
+        <SectionTitle>Refeições do Dia</SectionTitle>
+
+        <ScrollView horizontal={true}>
+          {new Array(4).fill(0).map((_, index) => (
+            <MealItem key={index} />
+          ))}
+        </ScrollView>
+      </LastMeals>
     </PageContainer>
   );
 }
